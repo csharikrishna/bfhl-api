f818b37f50c1425a678d790092c5e90c3a984a37
CHINNAPATTU S HARI KRISHNA <harikrishna.22bce9001@vitapstudent.ac.in>
2025-08-29 11:09:20 +0530

    Refactor server.js for improved readability

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.post('/bfhl', (req, res) => {
 
         const response = {
             is_success: true,
-            user_id: "csharikrishna",
+            user_id: "CHINNAPATTUS_HARIKRISHNA_18062005 ",
             email: "harikrishna.22bce9001@vitapstudent.ac.in",
             roll_number: "22BCE9001",
             odd_numbers: oddNumbers,
@@ -131,4 +131,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
 
